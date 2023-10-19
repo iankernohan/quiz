0972618124d1ca0d719647d55dc76aef9deb4f04
Ian <kernohani1@gmail.com>
2023-10-18 21:40:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Quiz from "./Quiz";
+import quizes from "./quizes";
+import QuizList from "./QuizList";
+import Header from "./Header";
+import Preview from "./Preview";
+import { useState } from "react";
+
 
 function App() {
+  const [isQuiz, setIsQuiz] = useState('');
+  const [hoverQuiz, setHoverQuiz] = useState(false);
+
+  const quiz = quizes[isQuiz]
+
+  function handleQuizHover(value) {
+    setHoverQuiz(value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <Header >Quisp</Header>
+      {!isQuiz ?
+        <>
+          <QuizList quizes={quizes} handleQuizSelect={name => setIsQuiz(name)} onQuizHover={handleQuizHover} />
+          {hoverQuiz && <Preview />}
+        </>
+        :
+        <Quiz title={quiz.name} questions={quiz.questions} setIsQuiz={setIsQuiz} />}
     </div>
-  );
+  )
 }
 
 export default App;
